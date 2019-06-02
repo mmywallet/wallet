28aa1ee2072a96df3765bde9cad1aec04f6f95d7
mmywallet <mmywallet@gmail.com>
2019-06-03 04:37:35 +0900

    Updates

---
+++ precache-manifest.423b5231c8dbaad0908cc62a18c2b383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d2b1ce4094f96629e16aacbb417195",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "4a44eb394c4107ddff9f",
+    "url": "/wallet/static/css/main.1b66bce9.chunk.css"
+  },
+  {
+    "revision": "ea6b57071ed6e634c7c3",
+    "url": "/wallet/static/js/2.fba2beee.chunk.js"
+  },
+  {
+    "revision": "4a44eb394c4107ddff9f",
+    "url": "/wallet/static/js/main.6233fb40.chunk.js"
+  },
+  {
+    "revision": "4313a70b9113ec012757",
+    "url": "/wallet/static/js/runtime~main.ee2bcf40.js"
+  },
+  {
+    "revision": "5f578ddfb8667975e755be40e6dfea54",
+    "url": "/wallet/static/media/wallet.5f578ddf.svg"
+  }
+]);
